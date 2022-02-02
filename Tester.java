94b4fa1cb1a1883ab661352c041fd293e9dc8c46
RyanM15 <77285830+RyanM15@users.noreply.github.com>
2022-02-02 18:40:04 -0500

    final tester

--- Tester.java
+++ Tester.java
@@ -5,6 +5,12 @@ public class Tester{
 	public static void main(String[] args){
 		Mastermind m1 = new Mastermind();
 		
+		System.out.println("Welcome to Mastermind!!");
+		System.out.println("Rules: There is a secret code made up of four letters that you must guess. You have ten turns to guess the code.");
+		System.out.println("After you guess a score will appear on the right side of the board. If you have a letter correct you will get a white pin.");
+		System.out.println("If you have a letter correct and in the correct position you will get a black pin. These will be represented by a 'B' and 'W'.");
+		System.out.println("The number of these pins you have will appear under the 'B' and 'W'.");
+		System.out.println("Have fun!!!");
 		
 		//System.out.println("Welcome to Mastermind!");
 		//System.out.println("Welcome to Mastermind!");
@@ -26,3 +32,4 @@ public class Tester{
 }
 		
 	
+
